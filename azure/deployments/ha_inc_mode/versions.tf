c30fb76282cf2d629b4d5774ec76a3329af06ad6
jpragyaj-svg <jpragyaj@gmail.com>
2026-02-09 12:05:48 +0000

    adding provider adc scripts for ha in inc mode
    Signed-off-by: jpragyaj-svg <jpragyaj@gmail.com>

---
+++ azure/deployments/ha_inc_mode/versions.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    citrixadc = {
+      source  = "citrix/citrixadc"
+      version = "2.1.0" # tested for 1.29.0 version. Should work for later versions as well.
+    }
+  }
+}
+
+data "terraform_remote_state" "infra" {
+  backend = "local"
+  config = {
+    path = "../ha_availability_set_single_ip/terraform.tfstate"
+  }
+}
+
+provider "citrixadc" {
+  endpoint = format("http://%s", data.terraform_remote_state.infra.outputs.public_nsips[0])
+  username = var.adc_admin_username
+  password = var.adc_admin_password
+}
+
+provider "citrixadc" {
+  alias    = "netscaler2"
+  endpoint = format("http://%s", data.terraform_remote_state.infra.outputs.public_nsips[1])
+  username = var.adc_admin_username
+  password = var.adc_admin_password
+}
