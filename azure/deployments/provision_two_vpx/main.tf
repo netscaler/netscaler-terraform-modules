c2b361eeabc98b8baf6a9e1db6e304b52e9f775d
jpragyaj-svg <jpragyaj@gmail.com>
2026-02-09 12:54:35 +0000

    have ports open for security group
    Signed-off-by: jpragyaj-svg <jpragyaj@gmail.com>

--- azure/deployments/provision_two_vpx/main.tf
+++ azure/deployments/provision_two_vpx/main.tf
@@ -103,7 +103,7 @@ resource "azurerm_network_security_rule" "terraform-allow-client-http-from-inter
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
-  destination_port_ranges     = ["80", "443"]
+  destination_port_ranges     = ["80", "443", "6379", "8443"]
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.terraform-resource-group.name
